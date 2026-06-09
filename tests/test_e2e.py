8bb169b7f550abb113f4cb202872ef155e248d51
1interprete1 <victor.hugo.queretaro@proton.me>
2026-06-09 17:06:08 +0000

    S0.6: integración e2e

---
+++ tests/test_e2e.py
@@ -0,0 +1,8 @@
+from datetime import datetime, timezone
+from comind import inbox, search, review
+
+def test_capturar_buscar_repasar(tmp_store):
+    ahora = datetime(2026, 6, 12, 9, 0, tzinfo=timezone.utc)
+    c = inbox.add("idea: capa ejecutiva sobre PKG", timestamp=ahora)
+    assert search.find("ejecutiva")[0].id == c.id
+    assert c.id in [x.id for x in review.today(now=ahora)]
